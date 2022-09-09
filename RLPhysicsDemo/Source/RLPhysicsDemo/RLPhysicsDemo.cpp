0204b521f7860acb7e480a965af1afa8899ef973
Josh <josh.church.josh@gmail.com>
2022-09-09 11:16:44 -0400

    starter project commit

---
+++ RLPhysicsDemo/Source/RLPhysicsDemo/RLPhysicsDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RLPhysicsDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RLPhysicsDemo, "RLPhysicsDemo" );
